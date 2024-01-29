0598ff890d340b5f07270e496b6ce0f5176f8641
Maxwell French <maxwell@frenchnet.org>
2024-01-29 16:51:26 -0500

    Add Aged Brie unit test, start refactor of UpdateQuality, bump to .net7.

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -6,13 +6,40 @@ namespace csharp
     [TestFixture]
     public class GildedRoseTest
     {
+        /// <summary>
+        /// Check Aged Brie special handling (1 cycle)
+        /// </summary>
         [Test]
-        public void foo()
+        public void AgedBrieValue1()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 25 } };
+
+            var rose = new GildedRose(items);
+
+            rose.UpdateQuality();
+            Assert.AreEqual("Aged Brie", items[0].Name);
+            Assert.AreEqual(9, items[0].SellIn);
+            Assert.AreEqual(26, items[0].Quality);
+        }
+
+        /// <summary>
+        /// Check Aged Brie special handling (30 cycles)
+        /// </summary>
+        [Test]
+        public void AgedBrieValue30()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 } };
+
+            var rose = new GildedRose(items);
+
+            for (var i = 0; i < 30; ++i)
+            {
+                rose.UpdateQuality();
+            }
+
+            Assert.AreEqual("Aged Brie", items[0].Name);
+            Assert.AreEqual(-20, items[0].SellIn);
+            Assert.AreEqual(50, items[0].Quality);
         }
     }
 }
